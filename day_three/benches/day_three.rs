deb6e4c43229ca833ac8ab7634e804c5c75af2aa
Carl <carllundin55@gmail.com>
2021-12-03 20:57:56 -0800

    Did a bit of optimizing, should use a matrix data structure to squeek out some more..

--- day_three/benches/day_three.rs
+++ day_three/benches/day_three.rs
@@ -7,26 +7,43 @@ fn calculate_power_consumption_large(c: &mut Criterion) {
     let mut file = File::open(DAY_THREE_INPUT.to_string()).unwrap();
     let mut contents = String::new();
 
+    let mut diagnostics = [[0; 12]; 1000];
+
     file.read_to_string(&mut contents).unwrap();
 
-    let diagnostics: Vec<i32> = contents
+    let temp_diagnostics: Vec<Vec<u32>> = contents
         .lines()
-        .map(|s| i32::from_str_radix(s, 2).unwrap())
+        .map(|v| v.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
-    let width = 12;
+
+    for (i, n) in temp_diagnostics.into_iter().enumerate() {
+        for (j, m) in n.into_iter().enumerate() {
+            diagnostics[i][j] = m;
+        }
+    }
+
     c.bench_function("calculate_power_consumption large", |b| {
-        b.iter(|| calculate_power_consumption(black_box(&diagnostics), black_box(width)))
+        b.iter(|| calculate_power_consumption(black_box(&diagnostics)))
     });
 }
 
 fn calculate_power_consumption_small(c: &mut Criterion) {
-    let diagnostics = vec![
-        0b00100, 0b11110, 0b10110, 0b10111, 0b10101, 0b01111, 0b00111, 0b11100, 0b10000, 0b11001,
-        0b00010, 0b01010,
+    let diagnostics = [
+        [0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 1, 1, 0],
+        [1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1],
+        [0, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1],
+        [1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 0],
     ];
-    let width = 5;
     c.bench_function("calculate_power_consumption small", |b| {
-        b.iter(|| calculate_power_consumption(black_box(&diagnostics), black_box(width)))
+        b.iter(|| calculate_power_consumption(black_box(&diagnostics)))
     });
 }
 
--- day_three/src/lib.rs
+++ day_three/src/lib.rs
@@ -1,21 +1,26 @@
 pub const DAY_THREE_INPUT: &str = "puzzle_input.txt";
 
-pub fn calculate_power_consumption(diagnostics: &[i32], width: usize) -> i32 {
+pub fn calculate_power_consumption<const N: usize, const M: usize>(
+    diagnostics: &[[u32; M]; N],
+) -> u32 {
     let mut gamma = 0;
     let mut epsilon = 0;
 
-    for i in (0..width).rev() {
+    let n = diagnostics.len();
+    let m = diagnostics[0].len();
+
+    for y in 0..m {
         let mut one_count = 1;
         let mut zero_count = 1;
-        for x in diagnostics {
-            let z = x >> i;
-            if z & 1 == 1 {
+        for x in 0..n {
+            let val = diagnostics[x][y];
+            if val == 1 {
                 one_count += 1;
             } else {
                 zero_count += 1;
             }
         }
-        
+
         gamma = gamma << 1;
         epsilon = epsilon << 1;
 
@@ -116,11 +121,21 @@ mod tests {
 
     #[test]
     fn test_example_power_consumption() {
-        let diagnostics = vec![
-            0b00100, 0b11110, 0b10110, 0b10111, 0b10101, 0b01111, 0b00111, 0b11100, 0b10000,
-            0b11001, 0b00010, 0b01010,
+        let diagnostics = [
+            [0, 0, 1, 0, 0],
+            [1, 1, 1, 1, 0],
+            [1, 0, 1, 1, 0],
+            [1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1],
+            [1, 1, 1, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 1, 0, 1, 0],
         ];
-        assert_eq!(198, calculate_power_consumption(&diagnostics, 5));
+        assert_eq!(198, calculate_power_consumption(&diagnostics));
     }
 
     #[test]
@@ -128,13 +143,21 @@ mod tests {
         let mut file = File::open(DAY_THREE_INPUT.to_string()).unwrap();
         let mut contents = String::new();
 
+        let mut diagnostics = [[0; 12]; 1000];
+
         file.read_to_string(&mut contents).unwrap();
 
-        let diagnostics: Vec<i32> = contents
+        let temp_diagnostics: Vec<Vec<u32>> = contents
             .lines()
-            .map(|s| i32::from_str_radix(s, 2).unwrap())
+            .map(|v| v.chars().map(|c| c.to_digit(10).unwrap()).collect())
             .collect();
-        assert_eq!(1131506, calculate_power_consumption(&diagnostics, 12));
+
+        for (i, n) in temp_diagnostics.into_iter().enumerate() {
+            for (j, m) in n.into_iter().enumerate() {
+                diagnostics[i][j] = m;
+            }
+        }
+        assert_eq!(1131506, calculate_power_consumption(&diagnostics));
     }
 
     #[test]
